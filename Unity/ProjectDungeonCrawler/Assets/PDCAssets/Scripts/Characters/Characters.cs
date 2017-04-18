af7cd2943fe2a1abd9998cfa396aa2e7978eb3ea
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-04-18 13:58:16 +0200

    started_on_player

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Characters/Characters.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PDC
+{
+    namespace Characters
+    {
+        public abstract class BaseCharacter : MonoBehaviour
+        {
+            public string characterName = "New Character";
+            public Stats characterStats;
+            List<Coroutine> statusEffects;
+
+            public void TakeDamage(float damage)
+            {
+                characterStats.currentHP -= damage;
+                if(characterStats.currentHP <= 0)
+                    Die();
+            }
+
+            public void GiveStatusEffect(Coroutine effect)
+            {
+                statusEffects.Add(effect);
+            }
+
+            public void CureEffects()
+            {
+                foreach(Coroutine c in statusEffects)
+                {
+                    StopCoroutine(c);
+                }
+            }
+
+            public abstract void Move();
+
+            public abstract void Attack();
+
+            public abstract void Die();
+        }
+
+        [SerializeField]
+        public class Stats
+        {
+            [SerializeField]
+            float maxHP = 100;
+            public float currentHP;
+            [SerializeField]
+            float maxSouls = 100;
+            public float currentSouls;
+            public float armorRating;
+            public float movementSpeed;
+
+            public float MaxHP
+            {
+                get
+                {
+                    return maxHP;
+                }
+            }
+
+            public float MaxSouls
+            {
+                get
+                {
+                    return maxSouls;
+                }
+            }
+        }
+    }
+}
---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/ScriptableObjects/StatusEffect.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PDC.Characters;
+
+public abstract class StatusEffect : ScriptableObject {
+    
+}
