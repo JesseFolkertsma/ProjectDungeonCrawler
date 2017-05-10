11b1cce7319942f0243352de26cd8ac15db43889
JanVijfhuizen <Jan.vijfhuizen@outlook.com>
2017-05-10 08:50:15 +0200

    Mapgenerator updated met difficulty optie

--- Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Map/MapManager.cs
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Map/MapManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour {
+    #region Old Map System
+    /*
     [SerializeField]
     private RectTransform playerIcon;
     [Tooltip("The maximal distance when you can interact with an item on the map"), SerializeField]
@@ -30,5 +32,23 @@ public class MapManager : MonoBehaviour {
 
         //nothing is happening with the place yet, so Im just going to load a random level
         SceneManager.LoadScene(loadLevelName);
+    }
+    */
+    #endregion
+
+    //map node system
+
+    
+
+    private void Awake()
+    {
+
+    }
+
+    private void InitializeMap() //make this a customeditor button
+    {
+        //so im going to use a few colored map to check three colors: black, grey, white
+        //black you cant walk, grey you can, and white you can walk faster
+        //create a duplicate of the map in those colors and somehow scan that picture
     }
 }
