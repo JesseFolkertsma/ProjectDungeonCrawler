fa9e40e3d424e89c561b7f057c72a52ab14f82cf
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-05-11 10:34:52 +0200

    playercontroller

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Characters/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PDC.Characters
+{
+    public class PlayerController : BaseCharacter
+    {
+        public static PlayerController instance;
+
+        void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(gameObject);
+        }
+
+        void Update()
+        {
+            CheckInput();
+            Move();
+        }
+
+        void CheckInput()
+        {
+            #region MovementInput
+            float xInput = Input.GetAxisRaw("Horizontal");
+            float yInput = Input.GetAxisRaw("Vertical");
+
+
+            #endregion
+        }
+
+        public override void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
