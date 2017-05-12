6cf0a68981352b68efddb5322febbb3afb2cb371
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-05-12 15:11:19 +0200

    almostFinishPlayer

--- Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Characters/PlayerController.cs
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Characters/PlayerController.cs
@@ -10,12 +10,21 @@ namespace PDC.Characters
         public static PlayerController instance;
 
         //Public variables
+        [HideInInspector]
         public Rigidbody rb;
+        public AudioSource audioS;
         public Transform camHolder;
         public Camera playerCam;
+        public Transform weaponPos;
+        public Transform weaponTrans;
         public HeadBobVariables headbobVariables;
+        public LayerMask playerLayer;
         public float movementSpeed;
+        public float acceleration;
+        public float movementModifier;
         public float mouseSensitivity;
+        public float jumpForce;
+        public AudioClip[] footSteps;
 
         //Private variables
         Vector3 direction;
@@ -25,15 +34,30 @@ namespace PDC.Characters
         float yInput;
         float xRot;
         float yRot;
+        float bobY;
+        float bobX;
+        float acc;
+        bool grounded;
+        bool bobUp;
 
         [Serializable]
         public struct HeadBobVariables
         {
+            public bool enableCameraEffects;
+            public bool mikeMode;
+            [Header("The intensity of headbobbing")]
+            public Vector2 headbobIntensity;
+            [Header("The speed of headbobbing")]
             public float maxBobSpeed;
+            [Header("The panning you get when strafing")]
             public float maxCameraPan;
+            [Header("How fast the camera pans")]
             public float panSpeed;
+            [Header("Standard camera FOV")]
             public float baseFOV;
-            public float maxFOV;
+            [Header("Extra FOV when running")]
+            public float fovBonus;
+            [Header("Interpolate speed between FOV")]
             public float changeFOVSpeed;
         }
 
@@ -45,18 +69,35 @@ namespace PDC.Characters
                 Destroy(gameObject);
 
             rb = GetComponent<Rigidbody>();
+            audioS = GetComponent<AudioSource>();
+
+            if (headbobVariables.mikeMode)
+            {
+                headbobVariables.headbobIntensity = new Vector2(.1f, .5f);
+                headbobVariables.maxBobSpeed = 7;
+                headbobVariables.maxCameraPan = 80;
+                headbobVariables.fovBonus = 50;
+            }
         }
 
         void Update()
         {
-            CheckInput();
-            CameraEffects();
+            if (!isdead)
+            {
+                CheckInput();
+                Checks();
+                CameraEffects();
+                WeaponEffects();
+            }
         }
 
         void FixedUpdate()
         {
-            Move();
-            Rotate();
+            if (!isdead)
+            {
+                Move();
+                Rotate();
+            }
         }
 
         void CheckInput()
@@ -75,18 +116,120 @@ namespace PDC.Characters
             mouseX = Vector3.up * xRot;
             mouseY = -(Vector3.right * yRot);
             #endregion
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+        }
+
+        void Checks()
+        {
+            if(Physics.Raycast(transform.position + Vector3.up, Vector3.down, 1.1f, playerLayer))
+            {
+                grounded = true;
+            }
+            else
+            {
+                grounded = false;
+            }
+        }
+
+        void WeaponEffects()
+        {
+            WeaponSway();
+        }
+
+        void WeaponSway()
+        {
+            Vector3 newPos = new Vector3(-xInput /5, -rb.velocity.y/20, 0);
+            weaponTrans.localPosition = Vector3.Lerp(weaponTrans.localPosition, newPos, Time.deltaTime * 2);
         }
 
         void CameraEffects()
         {
-            if(xInput != 0)
+            if (headbobVariables.enableCameraEffects)
             {
-                playerCam.transform.rotation = Quaternion.Lerp(playerCam.transform.rotation, camHolder.rotation, headbobVariables.panSpeed * Time.deltaTime);
+                //Stafe effects
+                if (xInput != 0)
+                {
+                    Quaternion newRot = camHolder.rotation;
+                    newRot.eulerAngles += new Vector3(0, 0, headbobVariables.maxCameraPan * -xInput);
+                    playerCam.transform.rotation = Quaternion.Lerp(playerCam.transform.rotation, newRot, headbobVariables.panSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    playerCam.transform.rotation = Quaternion.Lerp(playerCam.transform.rotation, camHolder.rotation, headbobVariables.panSpeed * Time.deltaTime);
+                }
+                //Walking forward effects
+                if (yInput > 0.1)
+                {
+                    //FOV effect
+                    playerCam.fieldOfView = Mathf.Lerp(playerCam.fieldOfView, headbobVariables.baseFOV + headbobVariables.fovBonus / movementModifier * acc, headbobVariables.changeFOVSpeed * Time.deltaTime);
+
+                    //Headbobbing
+                    Bobhead(1f);
+                }
+                else
+                {
+                    if (playerCam.fieldOfView != headbobVariables.baseFOV)
+                        playerCam.fieldOfView = Mathf.Lerp(playerCam.fieldOfView, headbobVariables.baseFOV, headbobVariables.changeFOVSpeed * 3 * Time.deltaTime);
+
+                    if (playerCam.transform.localPosition != Vector3.zero)
+                        playerCam.transform.localPosition = Vector3.Lerp(playerCam.transform.localPosition, Vector3.zero, headbobVariables.maxBobSpeed * Time.deltaTime);
+                }
+                //Walking backwards effects
+                if (yInput < -0.1)
+                {
+                    Bobhead(.5f);
+                }
+            }
+        }
+
+        void Bobhead(float multiplier)
+        {
+            //Calculate bob position
+            if (bobUp)
+            {
+                bobY = headbobVariables.headbobIntensity.y * multiplier;
+                if (playerCam.transform.localPosition.y >= bobY - .01)
+                {
+                    bobUp = false;
+                    Step();
+                }
             }
             else
             {
-                playerCam.transform.rotation = Quaternion.Lerp(playerCam.transform.rotation, camHolder.rotation, headbobVariables.panSpeed * Time.deltaTime);
+                bobY = -headbobVariables.headbobIntensity.y * multiplier;
+                if (playerCam.transform.localPosition.y <= bobY + .01)
+                {
+                    bobUp = true;
+                    bobX = UnityEngine.Random.Range(-headbobVariables.headbobIntensity.x, headbobVariables.headbobIntensity.x) * multiplier;
+                    Step();
+                }
             }
+
+            //Apply bob position
+            Vector3 newPos = new Vector3(bobX, bobY, 0);
+            playerCam.transform.localPosition = Vector3.MoveTowards(playerCam.transform.localPosition, newPos, (Time.deltaTime * headbobVariables.maxBobSpeed / movementModifier * acc) * multiplier);
+        }
+
+        void Step()
+        {
+            if (grounded)
+            {
+                if (audioS != null && footSteps.Length > 0)
+                {
+                    audioS.clip = footSteps[UnityEngine.Random.Range(0, footSteps.Length)];
+                    audioS.Play();
+                }
+            }
+        }
+
+        void Jump()
+        {
+            if(grounded)
+                rb.velocity += Vector3.up * jumpForce;
         }
 
         public override void Attack()
@@ -96,14 +239,25 @@ namespace PDC.Characters
 
         public override void Die()
         {
-            throw new NotImplementedException();
+            if (!isdead)
+            {
+                isdead = true;
+                playerCam.transform.parent = null;
+                playerCam.gameObject.AddComponent<CapsuleCollider>();
+                playerCam.gameObject.AddComponent<Rigidbody>();
+            }
         }
 
         public override void Move()
         {
             if(direction != Vector3.zero)
             {
-                rb.MovePosition(rb.position + (direction * Time.fixedDeltaTime * movementSpeed));
+                acc = Mathf.Lerp(acc, movementModifier, Time.fixedDeltaTime * acceleration);
+                rb.MovePosition(rb.position + (direction * Time.fixedDeltaTime * movementSpeed * acc));
+            }
+            else
+            {
+                acc = Mathf.Lerp(acc, 0f, Time.fixedDeltaTime * acceleration * 2);
             }
         }
 
