7baded12b79a93f0f1f045d99732eff90dbbd9f8
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-05-24 10:58:56 +0200

    icons

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/UI/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PDC.Weapons;
+using PDC.StatusEffects;
+
+namespace PDC.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager instance;
+
+        void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(gameObject);
+        }
+
+        public void UpdateWeaponVisual(List<Weapon> weapons, Weapon equipped)
+        {
+            if(equipped != null)
+                UpdateAmmo(equipped.weaponIcon, equipped.ammo);
+        }
+
+        public void UpdateAmmo(Sprite ammoSprite, int ammo)
+        {
+
+        }
+
+
+    }
+}
