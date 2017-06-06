74a9c94edbf5b67de736e2bf1eca703925ee35e6
JanVijfhuizen <Jan.vijfhuizen@outlook.com>
2017-06-06 14:34:08 +0200

    Pathfinding implemented (not working yet)

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/Pathfinding/Pathfinding_Bakeable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding_Bakeable : MonoBehaviour {
+
+    public float refreshSpeed;
+    public PathFinding.BakeType bakeType;
+    [HideInInspector]
+    public List<PathFinding.Node> myNodes = new List<PathFinding.Node>();
+    [HideInInspector]
+    public List<PathFinding.Node> oldNodes = new List<PathFinding.Node>();
+
+    private void Start()
+    {
+        if(bakeType == PathFinding.BakeType.Object)
+        {
+            Debug.Log("Object baking is not supported. Change baketype to something else.");
+            return;
+        }
+
+        PathFinding.self.BakeObjectRealTime(this);
+    }
+}
