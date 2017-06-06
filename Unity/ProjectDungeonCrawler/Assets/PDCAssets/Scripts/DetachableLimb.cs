5998e0826fe8139fb52bd1ce688c6f1a19de7786
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-06-06 13:40:01 +0200

    detachable_limbs

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Scripts/DetachableLimb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PDC;
+using PDC.StatusEffects;
+using System;
+
+[RequireComponent(typeof(CharacterJoint))]
+public class DetachableLimb : MonoBehaviour, IHitable{
+
+    bool isDetached = false;
+    CharacterJoint joint;
+
+    private void Awake()
+    {
+        joint = GetComponent<CharacterJoint>();
+    }
+
+    public void GetHit(float damage, EffectType hitType, StatusEffect[] effects, Vector3 shotPosition)
+    {
+        if (!isDetached)
+        {
+            isDetached = true;
+            transform.parent = null;
+            joint.breakForce = 0;
+        }
+        Vector3 direction = (transform.position - shotPosition).normalized;
+        GetComponent<Rigidbody>().AddForce(direction * 250 * 20);
+    }
+}
