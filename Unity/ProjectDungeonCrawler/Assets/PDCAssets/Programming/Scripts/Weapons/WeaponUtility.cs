1e9e1b834ac27b78d92799dd697af2cd2126d20b
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-10-04 15:38:46 +0200

    _Networkmanager

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/Weapons/WeaponUtility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WeaponUtility {
+
+    public static IHitable[] ShootWithRaycast(AttackType attackType, WeaponData weaponData)
+    {
+        switch (attackType)
+        {
+            case AttackType.Raycast:
+                return WURaycast(weaponData);
+            case AttackType.Hitbox:
+                return WUBoxCast(weaponData);
+        }
+        return new IHitable[0];
+    }
+
+    static IHitable[] WUBoxCast(WeaponData weaponData)
+    {
+        return new IHitable[0];
+    }
+
+    static IHitable[] WURaycast(WeaponData weaponData)
+    {
+        return new IHitable[0];
+    }
+}
+
+public enum AttackType
+{
+    Raycast,
+    Hitbox,
+    Projectile,
+};
