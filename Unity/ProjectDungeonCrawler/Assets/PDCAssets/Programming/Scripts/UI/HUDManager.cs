617c99c96d5bcfea3880af1ece2654dca1bbad4d
David Vink <david.vink@hotmail.com>
2017-10-11 15:48:17 +0200

    help

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/UI/HUDManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+    public Image hp;
+    public Transform ch;
+
+
+    public void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            HitMark();
+        }
+    }
+    public void UpdateHealth(float currentHP, float maxHP) {
+        hp.fillAmount = (currentHP / (maxHP / 100)) / 100;
+    }
+    public void HitMark() {
+        ch.GetChild(0).GetComponent<Animator>().SetTrigger("Hit");
+    }
+    
+}
