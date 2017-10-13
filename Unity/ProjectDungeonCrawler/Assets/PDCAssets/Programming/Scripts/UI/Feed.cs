4c8cf6904b6c88be3d0a1c3d7d8e4bcd5565d482
David Vink <david.vink@hotmail.com>
2017-10-13 14:22:44 +0200

    Killfeed ready for testing

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/UI/Feed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Feed : MonoBehaviour {
+    public GameObject feedPref;
+    public Transform feedWindow;
+
+    List<GameObject> fmList = new List<GameObject>();
+
+
+    private void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            FeedMessage("Hary gotz a kyl");
+        }
+    }
+    public void FeedMessage(string message) {
+        GameObject newFM = Instantiate(feedPref);
+        newFM.transform.SetParent(feedWindow, false);
+        newFM.transform.SetAsFirstSibling();
+        newFM.transform.GetChild(0).GetComponent<Text>().text = message;
+        fmList.Add(newFM);
+        
+    }
+    public void KillMessage(GameObject message) {
+        for(int i = 0; i < fmList.Count ; i++) {
+            if(fmList[i] == message) {
+                Destroy(fmList[i]);
+            }
+        }
+        fmList.Remove(message);
+    }
+}
