fec7cd7035ae0ed0f3bf6429f60b1745329624cd
David Vink <david.vink@hotmail.com>
2017-11-07 11:35:58 +0100

    Chat work

--- Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/Networking/NetworkedUI.cs
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/Networking/NetworkedUI.cs
@@ -5,18 +5,24 @@ using UnityEngine.Networking;
 
 
 public class NetworkedUI : NetworkBehaviour {
-
+#region General
     GeneralCanvas GC {
         get {
             return GeneralCanvas.canvas;
         }
     }
     NWPlayerCombat pc;
+
+    private void Update() {
+        FieldEndEdit();
+    }
+
     public void Init(NWPlayerCombat _pc){
         CmdFeedMessage(gameObject.name + " has joined the lobby!");
         pc = _pc;
 
     }
+#endregion
     #region Feed/Notifications
     [Command]
     public void CmdFeedMessage(string message) {
@@ -38,7 +44,7 @@ public class NetworkedUI : NetworkBehaviour {
         GC.SendMessage(message);
     }
     public void FieldEndEdit() {
-        if (Input.GetKeyDown(KeyCode.Return)) {
+        if (Input.GetKeyDown(KeyCode.Return) && !GC.inputField.isFocused) {
             CmdChatMessage(pc.objectName + ": " + GC.inputField.text);
             GC.inputField.text = "";
             GC.ToggleChat();
