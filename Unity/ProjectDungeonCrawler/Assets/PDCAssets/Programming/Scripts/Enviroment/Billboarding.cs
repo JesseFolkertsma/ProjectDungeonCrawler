7fea53ceb9081e1686fe12dfc53b2089054b22e2
David V <david.vink@hotmail.com>
2018-01-24 17:59:01 +0100

    billboarding

---
+++ Unity/ProjectDungeonCrawler/Assets/PDCAssets/Programming/Scripts/Enviroment/Billboarding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboarding : MonoBehaviour {
+
+	Transform player;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	void Update (){
+		var lookPos = player.position - transform.position;
+		lookPos.y = 0;
+		var rotation = Quaternion.LookRotation(lookPos);
+		transform.rotation = rotation;
+		transform.Rotate(90,0,-90);
+;	}
+}
